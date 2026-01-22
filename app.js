1cad4cb726d0b488d77e8e35ae5279fe5d4d907c
Domentia <micronetjunior@icloud.com>
2026-01-22 15:55:41 -0500

    Actualizar app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import fetch from "node-fetch";
 import { initMediasoup, createAnswerFromOffer } from "./routes/mediasoup.js";
-import "dotenv/config";
+//import "dotenv/config";
 
 const app = express();
 app.use(express.json());
